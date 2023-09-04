fd1442b6719d7356523545088e3b1177b3d5a88c
Your Name <thinhgiap76@gmail.com>
2023-09-04 12:46:20 +0700

    chat

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+  app.use(express.static(__dirname))
+  app.use(express.static('test'))
